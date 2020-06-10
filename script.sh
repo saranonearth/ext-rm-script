23e789253fef67c50a7d40aa1abc22a56baa1808
saranonearth <saran.suresh@outlook.com>
2020-06-11 01:34:46 +0530

    Fix: Extension variable

--- script.sh
+++ script.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
 read -p "File extension:" extension
 
-extension=${1?Error: No extension provided!}
+if [ -z "$extension" ];
+then
+    echo -e "${RED}No extension found. Please enter an extension"
+    exit $?
+fi
+echo "Starting at" `date`
+
 
 for FILE in */;
 do
     if [ -d "$FILE" ]; then
         cd $FILE
-        rm -rf *.extension
+        logger FILE_LIST
+        rm -rf *."$extension"
         cd ..
     fi
 done
 
-print_date()
-{
-    echo `date`
-    return
-}
-
-print_date
+echo -e "${GREEN}Done"
 
 exit $?
